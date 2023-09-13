a1ec99c54c14bff3182244de0369b8eb0fce14b3
thisis-arman <armanhossen1549@gmail.com>
2023-09-13 17:43:53 +0600

    dash header

--- karma-client/src/Layout/Dashboard/Dashboard.jsx
+++ karma-client/src/Layout/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
-
+import { MdOutlineDashboardCustomize } from 'react-icons/md'
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
@@ -28,36 +29,18 @@ const Dashboard = () => {
                                 <div className="border-t border-gray-100">
                                     <div className="px-2">
                                         <div className="py-4">
-                                            <a
-                                                href=""
+                                            <Link
+                                                to="/dashboard"
                                                 className="t group relative flex justify-center rounded bg-blue-50 px-2 py-1.5 text-blue-700"
                                             >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-5 w-5 opacity-75"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                                                    />
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                                    />
-                                                </svg>
+                                                <MdOutlineDashboardCustomize />
 
                                                 <span
                                                     className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100"
                                                 >
-                                                    General
+                                                    Tasks
                                                 </span>
-                                            </a>
+                                            </Link>
                                         </div>
 
                                         <ul className="space-y-1 border-t border-gray-100 pt-4">
@@ -210,9 +193,9 @@ const Dashboard = () => {
                     </ul>
 
                 </div>
-            </div>
+            </div >
 
-        </div>
+        </div >
     );
 };
 
