5e17292bcc39a022b1ff4f0da99b29cdc8a47437
thisis-arman <armanhossen1549@gmail.com>
2023-09-13 16:50:48 +0600

    newlatter and feature section added

--- karma-client/src/Component/Shared/Navbar.jsx
+++ karma-client/src/Component/Shared/Navbar.jsx
@@ -25,32 +25,32 @@ const Navbar = () => {
                             <nav aria-label="Global" className="hidden md:block">
                                 <ul className="flex items-center gap-6 text-sm">
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             About
                                         </a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             Careers
                                         </a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             History
                                         </a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             Services
                                         </a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             Projects
                                         </a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="/">
+                                        <a className="text-white transition hover:text-white/75 dark:text-white dark:hover:text-white/75" href="/">
                                             Blog
                                         </a>
                                     </li>
--- karma-client/src/Pages/HomePage/HomePage.jsx
+++ karma-client/src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import HeroBanner from "./HomeComponent/HeroBanner";
+import NewsletterCTA from "./HomeComponent/NewsletterCTA";
+import SpecialSection from "./HomeComponent/SpecialSection";
 
 
 const HomePage = () => {
     return (
         <div>
             <HeroBanner />
-
+            <SpecialSection />
+            <NewsletterCTA />
         </div>
     );
 };
